d51fdef9ce7454c0838d58df1ded76031f6f126a
Edwin Lam <165879372+edwin5354@users.noreply.github.com>
2024-09-30 18:01:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pickle
 
 # open csv
-df = pd.read_csv(r'C:\Users\Edwin\Python\bootcamp\Projects\SVM\csv\Student_Marks.csv')
+df = pd.read_csv('./csv/Student_Marks.csv')
 
 st.title('Support Vector Machine (SVM) Regression with Student Marks')
 st.write("This project focuses on conducting exploratory analysis to understand how specific variables impact student performance using Support Vector Machines (SVM). Additionally, it serves as an application for predicting student grades based on the number of subjects selected and the hours dedicated to studying.")
@@ -15,7 +15,7 @@ df['Performance'] = pd.qcut(df['Marks'], q=4, labels= ['Poor', 'Mid', 'Satisfact
 '''
 st.code(quantile_code, language="python")
 
-st.image(r'C:\Users\Edwin\Python\bootcamp\Projects\SVM\images\mark.png')
+st.image('./images/mark.png')
 
 hist_code = '''
 def marks():
@@ -30,7 +30,7 @@ st.code(hist_code, language='python')
 st.write("The chart above is a histogram that illustrates the distribution of students' marks, accompanied by a line chart for additional insights. The graph is left-skewed, indicating that, overall, students tend to perform poorly with lower scores.")
 
 st.write("To examine the impact of various factors on students' marks, here is a pairplot that illustrates the correlations between different variables and the outcomes.")
-st.image(r'C:\Users\Edwin\Python\bootcamp\Projects\SVM\images\pairplot.png')
+st.image('./images/pairplot.png')
 pair_code = '''
 def pairplot():
     sns.pairplot(df, hue="Performance", hue_order = ['Poor', 'Mid', 'Satisfactory', 'Good'], height=2.5)
@@ -42,7 +42,7 @@ st.code(pair_code, language= 'python')
 st.write("The dataset includes just two variables: the number of courses and study time. The plot demonstrates how these factors contribute to student marks. Notably, there is a strong positive and linear correlation between study time and student marks. Additionally, the data points are grouped according to student performance categories, as previously mentioned.")
 
 st.write("The plot below is a correlation matrix that illustrates the relationship between each variable and the output. It reveals a positive correlation of 0.42 between the number of subjects and student marks. More significantly, there is a very strong correlation of 0.94 between study hours and student marks, indicating that the more hours a student studies, the higher their marks tend to be.")
-st.image(r"C:\Users\Edwin\Python\bootcamp\Projects\SVM\images\corr_matrix.png")
+st.image("./images/corr_matrix.png")
 corr_code = """
 def corr_matrix():
     corr_df = df[['number_courses', 'time_study', 'Marks']]
@@ -56,7 +56,7 @@ corr_matrix()
 st.code(corr_code, language= 'python')
 
 st.write("Focusing on the correlation between study hours and student marks, a scatter plot has been created to illustrate this relationship. In the plot, the data points are categorized based on student performance levels, providing a clearer visualization of how different levels of study hours influence marks.")
-st.image(r"C:\Users\Edwin\Python\bootcamp\Projects\SVM\images\jointplot.png")
+st.image("./images/jointplot.png")
 
 join_code = """
 def jointplot():
@@ -68,7 +68,7 @@ jointplot()
 st.code(join_code, language= 'python')
 
 st.write("Finally, a regression analysis was conducted to create a trend line that represents the relationship between study hours and student marks. The plot displays both the slope and the R-squared value, indicating a strong positive correlation between these variables.")
-st.image(r"C:\Users\Edwin\Python\bootcamp\Projects\SVM\images\regression.png")
+st.image("./images/regression.png")
 reg_code = """
 def regression():
     p = sns.regplot(data=df, x="time_study", y="Marks")
@@ -145,7 +145,7 @@ def score(train_pred,test_pred,  is_optimized=False):
 '''
 st.code(acc_code, language='python')
 
-st.image(r"C:\Users\Edwin\Python\bootcamp\Projects\SVM\images\org_metrics.png")
+st.image("./images/org_metrics.png")
 metrics_code = '''
 def model_performance(df, is_optimised = False):
     # Set the bar width and positions
@@ -202,7 +202,7 @@ new_ytest_pred = tuned_reg.predict(X_test)
 '''
 st.code(tuned_code, language= 'python')
 
-st.image(r"C:\Users\Edwin\Python\bootcamp\Projects\SVM\images\tuned_metrics.png")
+st.image("./images/tuned_metrics.png")
 st.write("Here is the model's performance following hyperparameter tuning. As illustrated in the figure, the error decreases significantly. The optimized model will be saved for future predictions of student performance.")
 
 st.subheader('e) Prediction App')
@@ -232,8 +232,8 @@ def performance(mark):
     return comment
 
 # Open the saved models
-pickle_path = r"C:\Users\Edwin\Python\bootcamp\Projects\SVM\model\tuned_svm_model.pkl"
-pickle_scaler_path = r"C:\Users\Edwin\Python\bootcamp\Projects\SVM\model\scaler.pkl"
+pickle_path = "./model/tuned_svm_model.pkl"
+pickle_scaler_path = "./model/scaler.pkl"
 
 with open(pickle_path, 'rb') as file:
     saved_model = pickle.load(file)
